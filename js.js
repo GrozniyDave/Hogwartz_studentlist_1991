7daef1a286b65958bcc4535f102d331a50714fc2
GrozniyDave <38036338+GrozniyDave@users.noreply.github.com>
2019-03-03 14:05:38 +0100

    displaying list,filter it,expelling

---
+++ js.js
@@ -0,0 +1,138 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+//prototype declaration
+let StudentPrototype = {
+  fullname: "-student-fullname-",
+  house: "-house-"
+};
+
+//global variables
+let jsonData;
+let jsonDataBlood;
+let arrayStudents = [];
+let arrFiltered = [];
+let myJson;
+let spacebar;
+let fullLast;
+
+//initial function that calls json loading function
+function init() {
+  document.querySelector("#list").addEventListener("click", clickList);
+  loadJSON();
+}
+
+function loadJSON() {
+  /*fetches local json and cals function that creates array
+   of objects from json data*/
+  fetch("http://petlatkea.dk/2019/hogwarts/students.json")
+    .then(response => response.json())
+    .then(myJson => {
+      jsonData = myJson;
+      prepareObjects(myJson);
+    });
+}
+//makes array of student from json data and calls display function
+function prepareObjects(jsonData) {
+  jsonData.forEach(student => {
+    const studs = Object.create(StudentPrototype);
+    studs.fullname = student.fullname;
+    studs.house = student.house;
+    //push to array
+    arrayStudents.push(studs);
+    //puts unique id's to each student
+    arrayStudents.forEach(item => {
+      item.id = uuidv4();
+    });
+    arrFiltered = arrayStudents;
+  });
+  //IMPORTANT to not put it inside loop but after loop is finished!
+  displayStudent(arrayStudents);
+}
+//just to check status of array students
+console.log(arrayStudents);
+//when expell is clicked,call remove function
+function clickList(event) {
+  if (event.target.tagName == "BUTTON") {
+    clickRemove();
+  }
+}
+//remove function(expellling)
+function clickRemove() {
+  let target = event.target.dataset.id;
+  let indexToCut = arrayStudents.findIndex(student => student.id === target);
+  arrayStudents.splice(indexToCut, 1);
+  displayStudent(arrayStudents);
+}
+//displays student info in tbody html element
+function displayStudent() {
+  //IMPORTANT to clear before cloning!
+  document.querySelector("#list tbody").innerHTML = "";
+  arrFiltered.forEach(studs => {
+    //divide firstname and lastname
+    spacebar = studs.fullname.indexOf(" ");
+    let fullLast = studs.fullname.substring(spacebar, studs.fullname.length);
+    let firstName = studs.fullname.substring(0, spacebar);
+    //clone to html
+    const clone = document
+      .querySelector("template#student")
+      .content.cloneNode(true);
+    clone.querySelector("[data-field=name]").textContent = firstName;
+    clone.querySelector("[data-field=house]").textContent = studs.house;
+    clone.querySelector("[data-field=last]").textContent = fullLast;
+    clone.querySelector("button").dataset.id = studs.id;
+    document.querySelector("#list tbody").appendChild(clone);
+  });
+}
+//https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+//Modal stolen and modified
+document.addEventListener(
+  "click",
+  function(e) {
+    e = e || window.event;
+    let target = e.target || e.srcElement;
+
+    e.preventDefault();
+
+    if (
+      target.hasAttribute("data-toggle") &&
+      target.getAttribute("data-toggle") == "modal"
+    ) {
+      if (target.hasAttribute("data-target")) {
+        let m_ID = target.getAttribute("data-target");
+        document.getElementById(m_ID).classList.add("open");
+      }
+    }
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if (
+      (target.hasAttribute("data-dismiss") &&
+        target.getAttribute("data-dismiss") == "modal") ||
+      target.classList.contains("modal")
+    ) {
+      let modal = document.querySelector('[class="modal open"]');
+      modal.classList.remove("open");
+    }
+  },
+  false
+);
+//end of Modal
+
+function filtering(house) {
+  if (house === "all") {
+    arrFiltered = arrayStudents;
+  } else {
+    arrFiltered = arrayStudents.filter(elem => elem.house === house);
+    console.log(arrFiltered);
+  }
+
+  displayStudent();
+}
