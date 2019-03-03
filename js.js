a97b236fc2de43b4d81d8e17399fb22096715ed7
GrozniyDave <38036338+GrozniyDave@users.noreply.github.com>
2019-03-03 16:45:16 +0100

    added working modal for each stud

--- js.js
+++ js.js
@@ -14,14 +14,18 @@ let jsonData;
 let jsonDataBlood;
 let arrayStudents = [];
 let arrFiltered = [];
+let bloodFamily = [];
 let myJson;
+let myJsonBlood;
 let spacebar;
 let fullLast;
+let modal = document.querySelector("#simpleModal_5");
+let TrueLast;
 
 //initial function that calls json loading function
 function init() {
   document.querySelector("#list").addEventListener("click", clickList);
-  loadJSON();
+  loadJSONBlood();
 }
 
 function loadJSON() {
@@ -34,6 +38,17 @@ function loadJSON() {
       prepareObjects(myJson);
     });
 }
+
+function loadJSONBlood() {
+  fetch("http://petlatkea.dk/2019/hogwarts/families.json")
+    .then(response => response.json())
+    .then(myJsonBlood => {
+      jsonDataBlood = myJsonBlood;
+      console.log(myJsonBlood);
+      loadJSON();
+    });
+}
+
 //makes array of student from json data and calls display function
 function prepareObjects(jsonData) {
   jsonData.forEach(student => {
@@ -41,9 +56,25 @@ function prepareObjects(jsonData) {
     studs.fullname = student.fullname;
     studs.house = student.house;
     let spacebar = studs.fullname.indexOf(" ");
+    let TrueLast = studs.lastname.substring(
+      studs.lastname.indexOf(" "),
+      studs.lastname.length
+    );
+    studs.lastname = studs.fullname.substring(
+      spacebar + 1,
+      studs.fullname.length
+    );
+    studs.blood = TrueLast;
 
-    studs.lastname = studs.fullname.substring(spacebar, studs.fullname.length);
     studs.firstname = studs.fullname.substring(0, spacebar);
+    if (jsonDataBlood.half.includes(studs.lastname)) {
+      studs.blood = "half";
+    } else if (jsonDataBlood.pure.includes(studs.lastname)) {
+      studs.blood = "pure";
+    } else {
+      studs.blood = "muggle";
+    }
+
     //push to array
     arrayStudents.push(studs);
     //puts unique id's to each student
@@ -69,7 +100,11 @@ function clickRemove() {
   let target = event.target.dataset.id;
   let indexToCut = arrayStudents.findIndex(student => student.id === target);
   arrayStudents.splice(indexToCut, 1);
-  displayStudent(arrayStudents);
+
+  let indexToCut1 = arrFiltered.findIndex(student => student.id === target);
+  arrFiltered.splice(indexToCut1, 1);
+
+  displayStudent();
 }
 //displays student info in tbody html element
 function displayStudent() {
@@ -84,6 +119,10 @@ function displayStudent() {
     clone.querySelector("[data-field=house]").textContent = studs.house;
     clone.querySelector("[data-field=last]").textContent = studs.lastname;
     clone.querySelector("button").dataset.id = studs.id;
+    clone.querySelector(".Modallink").onclick = () => {
+      displayModal(studs);
+    };
+
     document.querySelector("#list tbody").appendChild(clone);
   });
 }
@@ -95,37 +134,31 @@ function uuidv4() {
     return v.toString(16);
   });
 }
+//displays modal
+function displayModal(stud) {
+  let spacebar = stud.lastname.indexOf(" ");
 
-//Modal stolen and modified
-document.addEventListener(
-  "click",
-  function(e) {
-    e = e || window.event;
-    let target = e.target || e.srcElement;
-    e.preventDefault();
-    if (
-      target.hasAttribute("data-toggle") &&
-      target.getAttribute("data-toggle") == "modal"
-    ) {
-      if (target.hasAttribute("data-target")) {
-        let m_ID = target.getAttribute("data-target");
-        document.getElementById(m_ID).classList.add("open");
-      }
-    }
-    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
-    if (
-      (target.hasAttribute("data-dismiss") &&
-        target.getAttribute("data-dismiss") == "modal") ||
-      target.classList.contains("modal")
-    ) {
-      let modal = document.querySelector('[class="modal open"]');
-      modal.classList.remove("open");
-    }
-  },
-  false
-);
-//end of Modal
+  document.querySelector("#simpleModal_5 > div > h1").textContent =
+    stud.fullname;
+  document.querySelector("#simpleModal_5 > div > img:nth-child(2)").src =
+    "images/" +
+    stud.lastname.substring(spacebar + 1, stud.lastname.length).toLowerCase() +
+    "_" +
+    stud.firstname.substring(0, 1).toLowerCase() +
+    ".png";
+  document.querySelector("#simpleModal_5 > div > img:nth-child(3)").src =
+    "flags/" + stud.house + ".png";
+  document.querySelector("#simpleModal_5 > div > h4").textContent =
+    "Blood type: " + stud.blood;
 
+  //shows modal on click
+  modal.style.display = "block";
+}
+//close modal on click
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+//filters
 function filtering(house) {
   if (house === "all") {
     arrFiltered = arrayStudents;
@@ -134,7 +167,7 @@ function filtering(house) {
   }
   displayStudent();
 }
-
+//sorts
 function sorting(property) {
   arrFiltered = arrFiltered.sort(sortDesc);
   function sortDesc(a, b) {
